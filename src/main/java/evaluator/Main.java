a77f2bd13ec3085bab2b67575e4aa9719259332b
m1ndl0ss <mindl0ss.dv@gmail.com>
2025-12-22 14:17:49 +0100

    try

--- src/main/java/evaluator/Main.java
+++ src/main/java/evaluator/Main.java
@@ -5,6 +5,7 @@ public class Main {
 
         System.out.println("LLM Safety Evaluator");
         System.out.println("test commit");
-        System.out.println("test");
+        System.out.println("hi");
+
     }
 }
