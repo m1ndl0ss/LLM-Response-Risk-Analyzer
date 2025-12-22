89cc410d4ed8ae93782a7799acc7cbc146bea0c3
m1ndl0ss <mindl0ss.dv@gmail.com>
2025-12-22 15:09:30 +0100

    promptLoader, gson

--- src/main/java/evaluator/PromptLoader.java
+++ src/main/java/evaluator/PromptLoader.java
@@ -1,5 +1,51 @@
 package main.java.evaluator;
 
-public class PromptLoader {
+import com.google.gson.*;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+
+public class PromptLoader {
+    private List<String> rolePlayPrompts = new ArrayList<>();
+    private List<String> instructionOverloadPrompts = new ArrayList<>();
+    private List<String> ethicalFramingPrompts = new ArrayList<>();
+
+    public PromptLoader(String filepath) throws IOException{
+        loadPrompts(filepath);
+    }
+
+    private void loadPrompts(String filepath) throws IOException{
+        JsonObject root = JsonParser.parseReader(new FileReader(filepath)).getAsJsonObject();
+        JsonArray categories = root.getAsJsonArray("categories");
+
+        for (JsonElement categoryElement : categories){
+            JsonObject category = categoryElement.getAsJsonObject();
+            String name = category.get("name").getAsString();
+            JsonArray prompts = category.getAsJsonArray("prompts");
+
+            for (JsonElement promptElement : prompts){
+                String prompt = promptElement.getAsString();
+
+                switch (name){
+                    case "role_play_override":
+                        rolePlayPrompts.add(prompt);
+                        break;
+                    case "instruction_override":
+                        instructionOverloadPrompts.add(prompt);
+                        break;
+                    case "ethical_framing":
+                        ethicalFramingPrompts.add(prompt);
+                        break;
+                    default:
+                        System.out.println("unknown category");
+                        break;
+                }
+            }
+        }
+    }
+    public List<String> getRolePlayPrompts(){return rolePlayPrompts;}
+    public List<String> getInstructionOverloadPrompts(){return instructionOverloadPrompts;}
+    public List<String> getEthicalFramingPrompts(){return ethicalFramingPrompts;}
 }
